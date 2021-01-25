9c7a972fb24434a585fd4a0bf5c4606ef3b508e0
Jonathan Dickins <42931047+jonathan-dickins@users.noreply.github.com>
2021-01-25 15:00:57 +0000

    Update lidar.R

--- lidar.R
+++ lidar.R
@@ -1,11 +1,3 @@
-install.packages("tidyverse")
-install.packages("purrr")
-install.packages("magick")
-install.packages("rayshader")
-install.packages("raster")
-install.packages("dplyr")
-install.packages("rgdal")
-
 
 #given a set of .asc terrain files and some latitude/longitude coordinates,
 #the following code produces 2d and 3d maps of the area with Rayshader
@@ -50,17 +42,9 @@ lat_long_to_other = function(lat,long, epsg_code) {
 }
 
 #create the boundaries for our map. Latlong data from Google Maps
-# Bournemouth Pier
-bottomleft = lat_long_to_other(50.713540, -1.882434, 27700)
-topright = lat_long_to_other(50.724410, -1.869810, 27700)
 
-#hh
 bottomleft = lat_long_to_other(50.708359, -1.777976, 27700)
 topright = lat_long_to_other(50.732398, -1.733914, 27700)
-
-#ph
-bottomleft = lat_long_to_other(50.650140, -2.089052, 27700)
-bottomright = lat_long_to_other(50.743724, -1.873141, 27700)
 
 # Create an extent object:
   e = extent(c(bottomleft[1], topright[1], bottomleft[2], topright[2]))
@@ -109,19 +93,3 @@ render_water(hh, zscale = 1, waterdepth = 3.1)
 
 #4m increase.
 render_water(hh, zscale = 1, waterdepth = 9.1)
-
-
-#old code
-hh %>%
-  sphere_shade(texture = "imhof1") %>%
-  add_shadow(ray_shade(hh, zscale = 1, multicore = TRUE, sunaltitude = 10, sunangle = -110),0.3) %>% 
-  add_water(detect_water(hh, zscale = 1, cutoff = 0.2,
-                         min_area = length(hh)/1500,
-                         max_height = 1.3)) %>%
-  
-  plot_3d(hh, zscale = 1, watercolor = "turqoise4", water = TRUE, waterdepth = 8, wateralpha = 0.8,
-          zoom=0.5, windowsize = 1000, 
-          background = "grey50", shadowcolor = "grey20", solid=TRUE, shadow = TRUE, soliddepth = -150, baseshape = "circle")
-
-
-
